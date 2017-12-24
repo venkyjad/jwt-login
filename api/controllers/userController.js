5ff184b8fcad351903cbe79522cdbe444182b583
venky <jadapalvdas@gmail.com>
2017-12-25 03:00:32 +0530

    changed secret key

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -28,7 +28,7 @@ exports.sign_in = function(req, res) {
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(401).json({ status:'failed',message: 'Authentication failed. Invalid user or password.' });
     }
-    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'VOKALSECRET') });
   });
 };
 
